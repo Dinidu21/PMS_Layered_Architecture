a71bae1d90e77e1fb06beefb0af2d1bc51323c54
Dinidu21 <dinidusachintha3@gmail.com>
2025-01-27 15:14:34 +0530

    9 Working

---
+++ src/main/java/com/dinidu/lk/pmt/bo/custom/Impl/ProjectsBOImpl.java
@@ -0,0 +1,116 @@
+package com.dinidu.lk.pmt.bo.custom.Impl;
+
+import com.dinidu.lk.pmt.bo.custom.ProjectsBO;
+import com.dinidu.lk.pmt.dao.DAOFactory;
+import com.dinidu.lk.pmt.dao.custom.ProjectDAO;
+import com.dinidu.lk.pmt.dto.ProjectDTO;
+import com.dinidu.lk.pmt.entity.Project;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class ProjectsBOImpl implements ProjectsBO {
+
+    ProjectDAO projectDAO =
+            (ProjectDAO) DAOFactory.getDaoFactory().
+                    getDAO(DAOFactory.DAOTypes.PROJECTS);
+
+    @Override
+    public String getProjectIdByName(String selectedProjectName) throws SQLException, ClassNotFoundException {
+       return projectDAO.getProjectIdByName(selectedProjectName);
+    }
+
+    @Override
+    public Map<String, String> getAllProjectNames() throws SQLException, ClassNotFoundException {
+        return projectDAO.getAllNames();
+    }
+
+    @Override
+    public String getProjectNameById(String projectId) throws SQLException, ClassNotFoundException {
+        return projectDAO.getProjectNameById(projectId);
+    }
+
+    @Override
+    public String getProjectIdByTaskId(long l) throws SQLException, ClassNotFoundException {
+        return projectDAO.getProjectIdByTaskId(l);
+    }
+
+    @Override
+    public boolean insert(ProjectDTO project) throws SQLException, ClassNotFoundException {
+        Project projectEntity = convertToEntity(project);
+        return projectDAO.insert(projectEntity);
+    }
+
+    @Override
+    public List<ProjectDTO> fetchAll() throws SQLException, ClassNotFoundException {
+        ArrayList<ProjectDTO> projectDTOS = new ArrayList<>();
+        List<Project> projects = projectDAO.fetchAll();
+        for (Project project : projects) {
+            projectDTOS.add(new ProjectDTO(
+                    project.getId(),
+                    project.getName(),
+                    project.getDescription(),
+                    project.getStartDate(),
+                    project.getEndDate(),
+                    project.getStatus(),
+                    project.getPriority(),
+                    project.getVisibility(),
+                    project.getCreatedBy(),
+                    project.getCreatedAt(),
+                    project.getUpdatedAt()
+            ));
+        }
+        return projectDTOS;
+    }
+
+    @Override
+    public List<ProjectDTO> searchProjectsByName(String searchQuery) throws SQLException, ClassNotFoundException {
+        List<Project> projects = projectDAO.searchByName(searchQuery);
+        List<ProjectDTO> projectDTOS = new ArrayList<>();
+        for (Project project : projects) {
+            projectDTOS.add(new ProjectDTO(
+                    project.getId(),
+                    project.getName(),
+                    project.getDescription(),
+                    project.getStartDate(),
+                    project.getEndDate(),
+                    project.getStatus(),
+                    project.getPriority(),
+                    project.getVisibility(),
+                    project.getCreatedBy(),
+                    project.getCreatedAt(),
+                    project.getUpdatedAt()
+            ));
+        }
+        return projectDTOS;
+    }
+
+    @Override
+    public List<ProjectDTO> getAllProjects() throws SQLException, ClassNotFoundException{
+        return projectDAO.getAllProjects();
+    }
+
+    @Override
+    public void updateProject(ProjectDTO projectDTO) throws SQLException, ClassNotFoundException {
+        Project project = convertToEntity(projectDTO);
+        projectDAO.updateProject(project);
+    }
+
+    private Project convertToEntity(ProjectDTO projectDTO) {
+        Project project = new Project();
+        project.setId(projectDTO.getId());
+        project.setName(projectDTO.getName());
+        project.setDescription(projectDTO.getDescription());
+        project.setStartDate(projectDTO.getStartDate());
+        project.setEndDate(projectDTO.getEndDate());
+        project.setStatus(projectDTO.getStatus());
+        project.setPriority(projectDTO.getPriority());
+        project.setVisibility(projectDTO.getVisibility());
+        project.setCreatedBy(projectDTO.getCreatedBy());
+        project.setCreatedAt(projectDTO.getCreatedAt());
+        project.setUpdatedAt(projectDTO.getUpdatedAt());
+        return project;
+    }
+}
